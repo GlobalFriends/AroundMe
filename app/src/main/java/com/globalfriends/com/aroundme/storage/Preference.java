c6bc14b93f51b9fd6cb2939e320181f5a03eac12
swapna <globalfriends747@gmail.com>
2015-12-01 20:15:24 -0800

    First test commit from Swapna

--- app/src/main/java/com/globalfriends/com/aroundme/storage/Preference.java
+++
@@ -1,8 +0,0 @@
-package com.globalfriends.com.aroundme.storage;
-
-/**
- * Created by vishal on 11/20/2015.
- */
-public class Preference {
-    private static final String FILE_NAME = "AroundMe";
-}
