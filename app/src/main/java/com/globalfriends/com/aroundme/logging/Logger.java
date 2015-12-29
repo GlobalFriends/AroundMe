68d2d8d24415f6fb8380b5cc20f64acc106d8ba7
swapna <globalfriends747@gmail.com>
2015-12-28 20:13:09 -0800

    Added code for fav layout

--- app/src/main/java/com/globalfriends/com/aroundme/logging/Logger.java
+++ app/src/main/java/com/globalfriends/com/aroundme/logging/Logger.java
@@ -1,16 +1,22 @@
-package com.globalfriends.com.aroundme.logging;
+package com.globalfriends.com.aroundme.Logging;
 
 import android.util.Log;
 
 /**
- * Created by anup on 11/10/15.
+ * Created by vishal on 11/15/2015.
  */
 public class Logger {
-    public static void e(String tag, String message) {
-        Log.e(tag, message);
+    private static final String TAG = "AroundMe";
+
+    public static void i(final String tag, final String message) {
+        Log.i(TAG + "tag", message);
     }
 
-    public static void i(String tag, String message) {
-        Log.i(tag, message);
+    public static void e(final String tag, final String message) {
+        Log.e(TAG + "tag", message);
+    }
+
+    public static void v(final String tag, final String message) {
+        Log.v(TAG + "tag", message);
     }
 }
